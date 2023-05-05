98b0a8646b01c6ef861ddb4100e7a077d5d67fdc
Lucija Krsticevic <lucija.krsticevic99@gmail.com>
2023-05-05 18:30:41 +0200

    Dodane opcije za unos novih poruka i za ispis vaznih i svih poruka

--- src/App.js
+++ src/App.js
@@ -1,15 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import Poruka from "./components/Poruka";
 
-const App = ({poruke}) => {
+const App = (props) => {
+    const [ poruke, postaviPoruke ] = useState(props.poruke)
+    const [ unosPoruke, postaviUnos ] = useState('Unesi poruku...')
+    const [ ispisSve, postaviIspis ] = useState(true)
+
+    const novaPoruka = (e) => {
+        e.preventDefault()
+        console.log('Klik', e.target)
+        const noviObjekt = {
+            id: poruke.length + 1,
+            sadrzaj: unosPoruke,
+            datum: new Date().toISOString(),
+            vazno: Math.random() > 0.5
+        }
+        postaviPoruke(poruke.concat(noviObjekt))
+        postaviUnos('')
+    }
+
+    const promjenaUnosa = (e) => {
+        console.log(e.target.value);
+        postaviUnos(e.target.value)
+    }
+
+    const porukeZaIspis = ispisSve 
+    ? poruke 
+    : poruke.filter(poruka => poruka.vazno === true)
+
     return (
       <div>
         <h1>Poruke</h1>
         <ul>
-          {poruke.map( p => 
+          {porukeZaIspis.map( p => 
             <Poruka key={p.id} poruka={p} />        
           )}
         </ul>
+        <form onSubmit={novaPoruka}>
+            <input value={unosPoruke} onChange={promjenaUnosa}/>
+            <button type="submit">Spremi</button>
+        </form>
+        <button onClick={() => postaviIspis(!ispisSve)}>
+            Prikazi {ispisSve ? "važne" : "sve"}
+        </button>
       </div>
     )
   }
